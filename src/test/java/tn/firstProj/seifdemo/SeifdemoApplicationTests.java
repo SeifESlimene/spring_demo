ee3f3e08fe8f8d2712c833f60e598672299d1d6f
seifeslimene <s.slimene19@gmail.com>
2023-02-10 21:37:54 +0100

    initial commit

---
+++ src/test/java/tn/firstProj/seifdemo/SeifdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.firstProj.seifdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeifdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
